8bcd0a3bfc07bc5a70cc88b70c5de1c1390c3f14
AdityaChafale05 <adityachafale2006@gmail.com>
2026-06-14 15:24:59 +0530

    sample data - for student table

---
+++ Placement_portal_database/sample_data.sql
@@ -0,0 +1,6 @@
+use placement_portal ;
+
+-- values for Students 
+-- Try inserting a 10-digit number starting with 6-9
+INSERT INTO Student (StudID, Roll_Number, Name, Branch, Email, Contact, CGPA, SSC_percent, HSC_percent, Active_backlog, Graduation_year)
+VALUES (121, 'CS2027006', 'Test', 'CSE', 'test@gmail.com', '9876543210', 8.00, 85.00, 88.00, FALSE, 2027);   
